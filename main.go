1f8147a8cd9e0c94d19403497d3e3b270a96a601
ivanSaichkin <ivan.saichkin@bk.ru>
2025-04-22 21:58:23 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type stringMap = map[string]string
+
+func main() {
+	notes := stringMap{}
+
+	fmt.Println("Application for notes")
+
+Menu:
+	for {
+		variant := GetMenu()
+		switch variant {
+		case 1:
+			PrintNote(notes)
+		case 2:
+			notes = AddNote(notes)
+		case 3:
+			notes = DeleteNote(notes)
+		case 4:
+			break Menu
+		}
+	}
+}
+
+func GetMenu() int64 {
+	var choice int64
+
+	fmt.Println("Choose func:")
+	fmt.Println("1. Print notes")
+	fmt.Println("2. Add note")
+	fmt.Println("3. Delete note")
+	fmt.Println("4. Exit")
+
+	fmt.Scan(&choice)
+
+	return choice
+}
+
+func PrintNote(notes stringMap) {
+	if len(notes) == 0 {
+		fmt.Println("No notes")
+	}
+	for key, value := range notes {
+		fmt.Println(key, ": ", value)
+	}
+}
+
+func AddNote(notes stringMap) stringMap {
+	var newNoteKey string
+	var newNoteValue string
+
+	fmt.Print("Enter note name: ")
+	fmt.Scan(&newNoteKey)
+	fmt.Print("Enter note: ")
+	fmt.Scan(&newNoteValue)
+
+	notes[newNoteKey] = newNoteValue
+
+	return notes
+}
+
+func DeleteNote(notes stringMap) stringMap {
+	var noteKeyForDelete string
+
+	fmt.Print("Enter note name: ")
+	fmt.Scan(&noteKeyForDelete)
+	delete(notes, noteKeyForDelete)
+
+	return notes
+}
